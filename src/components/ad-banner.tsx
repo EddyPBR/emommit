0e6d1bb875a70779a6fa3f05152fbc37a0af1c9e
EddyPBR <edvaldojunior1310@gmail.com>
2025-04-05 18:39:01 -0300

    🐛 try-2 fix google adsense load in production

--- src/components/ad-banner.tsx
+++ src/components/ad-banner.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Card, CardContent } from "./ui/card";
 import { env } from "@/configs/env";
 
@@ -9,35 +9,47 @@ interface AdBannerProps {
 }
 
 export function AdBanner({ className }: AdBannerProps) {
-	useEffect(() => {
-		const timeout = setTimeout(() => {
-			try {
-				// biome-ignore lint: suspicious/noAssignInExpressions: This is a workaround to bypass a TypeScript error related to the use of window object.
-				(window.adsbygoogle = window.adsbygoogle || []).push({});
-			} catch (e) {
-				console.error("Erro ao carregar o anúncio:", e);
-			}
-		}, 200); // espera layout montar
+	const adRef = useRef<HTMLDivElement>(null);
 
-		return () => clearTimeout(timeout);
+	useEffect(() => {
+		if (!adRef.current) return;
+
+		const observer = new ResizeObserver(([entry]) => {
+			const width = entry.contentRect.width;
+			if (width > 0) {
+				try {
+					// biome-ignore lint: suspicious/noAssignInExpressions: This is a workaround to bypass a TypeScript error related to the use of window object.
+					(window.adsbygoogle = window.adsbygoogle || []).push({});
+					observer.disconnect(); // só precisa chamar uma vez
+				} catch (e) {
+					console.error("Erro ao carregar o anúncio:", e);
+				}
+			}
+		});
+
+		observer.observe(adRef.current);
+
+		return () => observer.disconnect();
 	}, []);
 
 	return (
 		<Card className={`overflow-hidden ${className}`}>
 			<CardContent className="p-4">
-				<ins
-					className="adsbygoogle"
-					style={{
-						display: "block",
-						minHeight: 150,
-						maxHeight: 300,
-						width: "100%",
-					}}
-					data-ad-client={`ca-pub-${env.ADSENSE_CLIENT_ID}`}
-					data-ad-slot={env.ADSENSE_SLOT} // substitua pelo seu ad-slot
-					data-ad-format="auto"
-					data-full-width-responsive="true"
-				/>
+				<div ref={adRef}>
+					<ins
+						className="adsbygoogle"
+						style={{
+							display: "block",
+							width: "100%",
+							minHeight: 150,
+							maxHeight: 300,
+						}}
+						data-ad-client={`ca-pub-${env.ADSENSE_CLIENT_ID}`}
+						data-ad-slot={env.ADSENSE_SLOT}
+						data-ad-format="auto"
+						data-full-width-responsive="true"
+					/>
+				</div>
 			</CardContent>
 		</Card>
 	);
