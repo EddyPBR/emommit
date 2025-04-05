0275ee243ab7727b15b0c867f197d2b87b7423df
EddyPBR <edvaldojunior1310@gmail.com>
2025-04-05 19:46:08 -0300

    🔧 config script adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,13 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
 	return (
 		<html lang="pt-BR" suppressHydrationWarning>
 			<head>
-				<AdSendScript />
+				{process.env.NODE_ENV === "production" && (
+					<script
+						async
+						src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+						crossOrigin="anonymous"
+					/>
+				)}
 			</head>
 			<body className={`${fireCode.className} antialiased`}>
 				<ThemeProvider
