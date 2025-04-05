78a17a7ce49925ecc46d5d25b1058fdd5c2b2e86
EddyPBR <edvaldojunior1310@gmail.com>
2025-04-05 19:40:58 -0300

    🪛 adjusting ad-sense script strategy from 'afterInteractive' to 'lazyOnload' to improve performance and reduce initial load time.

--- src/components/ad-sense-script.tsx
+++ src/components/ad-sense-script.tsx
@@ -2,12 +2,12 @@ import { env } from "@/configs/env";
 import Script from "next/script";
 
 export function AdSendScript() {
-	if (process.env.NODE_ENV !== "production") return;
+	if (process.env.NODE_ENV !== "production") return null;
 
 	return (
 		<Script
 			id="adsense-script"
-			strategy="afterInteractive"
+			strategy="lazyOnload"
 			async
 			src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${env.ADSENSE_CLIENT_ID}`}
 			crossOrigin="anonymous"
